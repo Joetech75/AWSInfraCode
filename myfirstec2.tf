9921d0a3db7cd86b21e94962e058df90dc286ca0
joetech75 <joemtech75@gmail.com>
2019-12-22 16:20:43 +0800

    added count variable

--- myfirstec2.tf
+++ myfirstec2.tf
@@ -6,6 +6,7 @@ provider "aws"{
 #create a ec2 instance 
 
 resource "aws_instance" "myfirstec2" {
+  count = 6
   ami           = "ami-01112b32e723178b2"
   instance_type = "t2.micro"
   availability_zone = "ap-southeast-1a"
