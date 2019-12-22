7ecbc765f3932cd0d36257903b7b04d26b2e9eb5
joetech75 <joemtech75@gmail.com>
2019-12-22 16:11:59 +0800

    adding first file

---
+++ myfirstec2.tf
@@ -0,0 +1,14 @@
+provider "aws"{ 
+  region = "ap-southeast-1"
+  
+}
+
+#create a ec2 instance 
+
+resource "aws_instance" "myfirstec2" {
+  ami           = "ami-01112b32e723178b2"
+  instance_type = "t2.micro"
+  availability_zone = "ap-southeast-1a"
+  tags = {"Name": "myfirstec2Instance","env": "dev"}
+}
+
