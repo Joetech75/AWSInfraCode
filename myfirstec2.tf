e7beaac81d313f363dfb15e11e09a1384c65d493
joetech75 <joemtech75@gmail.com>
2019-12-29 12:46:30 +0800

    changed few codes in ec2

--- myfirstec2.tf
+++ myfirstec2.tf
@@ -1,11 +1,11 @@
-/*
+
 #create a ec2 instance 
-resource "aws_instance" "myfirstec2" {
+resource "aws_instance" "devec2" {
   ami               = var.server_amis   
   instance_type     = var.machine_type  
   availability_zone = var.az  
-  #subnet_id = "aws_subnet.public_subnet" 
+  subnet_id = "aws_subnet.public_subnet" 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags              = { "Name" : "myfirstec2Instance", "env" : "dev" }
 }
-*/
+ 
