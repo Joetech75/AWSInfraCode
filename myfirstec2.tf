aaaada76158137cb9bf045c117aaeeaaabd0bf12
joetech75 <joemtech75@gmail.com>
2020-01-04 18:14:26 +0800

    added another ec2 instance

--- myfirstec2.tf
+++ myfirstec2.tf
@@ -1,11 +1,19 @@
 
 #create a ec2 instance 
-resource "aws_instance" "devec2" {
-  ami               = var.server_amis   
+resource "aws_instance" "myfirstec2" {
+  ami               = var.server_ami1   
   instance_type     = var.machine_type  
   availability_zone = var.az  
-  subnet_id = "aws_subnet.public_subnet" 
+  #subnet_id = "aws_subnet.public_subnet" 
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags              = { "Name" : "myfirstec2Instance", "env" : "dev" }
 }
- 
+
+resource "aws_instance" "secondec2" {
+  ami               = var.server_ami2  
+  instance_type     = var.machine_type  
+  availability_zone = var.az  
+  #subnet_id = "aws_subnet.public_subnet" 
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags              = { "Name" : "mysecondec2Instance", "env" : "test" }
+}
